a1c4ef520a9d26d667f0a391e09daa1053d36c63
grajol3 <wiktorg6655@wp.pl>
2026-02-17 20:47:27 +0100

    fix login path

--- src/TenantLogin.js
+++ src/TenantLogin.js
@@ -14,7 +14,7 @@ export default function TenantLogin() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://atlanta-app.onrender.com/api/loginTEST", {
+      const response = await fetch("https://atlanta-app.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: email, password: pass }),
